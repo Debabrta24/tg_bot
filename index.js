cff92ff416651af0a18bd2a79b53d4275c20d374
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-04-02 03:22:26 +0530

    otj

--- index.js
+++ index.js
@@ -286,3 +286,14 @@ bot.action(/reject_(.+)/, async (ctx) => {
 // ===== RUN =====
 bot.launch();
 console.log("🤖 Bot running...");
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running...");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
